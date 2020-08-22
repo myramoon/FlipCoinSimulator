686a45dea2c8f7e4fb95ca1b91e764c69222e770
anamfazal <anamfazal94@gmail.com>
2020-08-22 15:29:41 +0530

    Refactors code to run till either has appeared 21 times

--- flip_Coin_Simulator.sh
+++ flip_Coin_Simulator.sh
@@ -7,19 +7,24 @@ tails=0
 
 #MAIN PROGRAM
 
-read -p "Enter number of times you want to flip: " fliptimes
-
-for (( count=0; count<$fliptimes; count++))	#for loop to run multiple simulations
+while [[ $heads -lt 21 && $tails -lt 21 ]]      #While loop that runs until either one has appeared 21 times
 do
-	flip_Result=$((RANDOM%2))		#get flip result for current coin flip
+	flip_Result=$((RANDOM%2))							#get flip result for current coin flip
 
 	if [[ $flip_Result -eq 1 ]]
 	then
-		heads=$((heads + 1))		#increment heads if flip result is 1
+		heads=$((heads + 1))								#increment heads if flip result is 1
 	else 
-		tails=$((tails + 1))		#increment tails if flip result is 0
+		tails=$((tails + 1))								#increment tails if flip result is 0
 	fi
 done
 
-echo "Number of times HEADS appeared: " $heads  #Print results of heads and tails
-echo "Number of times TAILS appeared: " $tails
+if [[ $heads -gt $tails ]]
+then
+	echo "HEADS won by" $((heads - tails ))   	#Print results of winner and margin of win
+elif [[ $tails -gt $heads ]]
+then
+	echo "TAILS won by" $((tails - heads ))
+else
+	echo "TIE with points= " $heads           	#Print tie with points
+fi
