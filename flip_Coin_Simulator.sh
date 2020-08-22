b36ea5290e53999ea60b715a8486a63355a3d9a3
anamfazal <anamfazal94@gmail.com>
2020-08-22 14:31:26 +0530

    Refactors code to run multiple flip simulations

--- flip_Coin_Simulator.sh
+++ flip_Coin_Simulator.sh
@@ -1,10 +1,25 @@
-#!/usr/local/bin/bash
+#!/usr/local/bin/bash 
 
-flip_Result=$((RANDOM%2))
+#VARIABLES
 
-if [[ $flip_Result -eq 1 ]]
-then
-	echo "Result of flip: HEADS"
-else 
-	echo "Result of flip: TAILS"
-fi
+heads=0
+tails=0
+
+#MAIN PROGRAM
+
+read -p "Enter number of times you want to flip: " fliptimes
+
+for (( count=0; count<$fliptimes; count++))	#for loop to run multiple simulations
+do
+	flip_Result=$((RANDOM%2))		#get flip result for current coin flip
+
+	if [[ $flip_Result -eq 1 ]]
+	then
+		heads=$((heads + 1))		#increment heads if flip result is 1
+	else 
+		tails=$((tails + 1))		#increment tails if flip result is 0
+	fi
+done
+
+echo "Number of times HEADS appeared: " $heads  #Print results of heads and tails
+echo "Number of times TAILS appeared: " $tails
